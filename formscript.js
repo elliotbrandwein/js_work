af05836167d708943314e8abdaec3c61e19b74f5
Elliot Brandwein <elliotbrandwein1@gmail.com>
2016-06-03 12:55:02 -0400

    password check is working

--- formscript.js
+++ formscript.js
@@ -1,9 +1,9 @@
  document.getElementById('submit_button').onclick= function ()
  {
  	var x =document.getElementById('form')
- 	var name = x.element[0].value;
- 	var email= x.element[1].value;
- 	var pass = x.element[2].value;
+ 	var name = x.elements[0].value;
+ 	var email= x.elements[1].value;
+ 	var pass = x.elements[2].value;
  	if(pass!='12345678')alert("invalid password");
  	if(isNumeric(name))alert("invalid name");
  	return false;
