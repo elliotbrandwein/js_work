3a1b16e127d59910fb5463900851a50dd0605a8a
Elliot Brandwein <elliotbrandwein1@gmail.com>
2016-06-03 14:04:25 -0400

    now has a button, and a input type button

--- formscript.js
+++ formscript.js
@@ -1,6 +1,6 @@
  document.getElementById('submit_button').onclick= function ()
  {
- 	var x =document.getElementById('form')
+ 	var x =document.getElementById('form');
  	var name = x.elements[0].value;
  	var email= x.elements[1].value;
  	var pass = x.elements[2].value;
@@ -10,15 +10,19 @@
  	return false;
 
  }
- function isNumeric(n) {
+ function isNumeric(n)
+ {
  	var i =0;
- 	while(i<n.length){
+ 	while(i<n.length)
+ 	{
  		if(checkForNum(n.charAt(i)))return true;
  		i++;
  	}
  	return false;
 }
  console.log("working");
-function checkForNum(n){
+
+function checkForNum(n)
+{
 	return !isNaN(parseFloat(n)) && isFinite(n);
 }
