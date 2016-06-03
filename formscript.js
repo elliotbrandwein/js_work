7cba160e74adae36632755d33a8b4910c4804d63
Elliot Brandwein <elliotbrandwein1@gmail.com>
2016-06-03 13:07:59 -0400

    is now fully working

--- formscript.js
+++ formscript.js
@@ -5,11 +5,20 @@
  	var email= x.elements[1].value;
  	var pass = x.elements[2].value;
  	if(pass!='12345678')alert("invalid password");
- 	if(isNumeric(name))alert("invalid name");
+ 	console.log(name);
+ 	if(!isNumeric(name))alert("invalid name");
  	return false;
 
  }
  function isNumeric(n) {
-  return !isNaN(parseFloat(n)) && isFinite(n);
+ 	var i =0;
+ 	while(i<n.length){
+ 		if(checkForNum(n.charAt(i)))return true;
+ 		i++;
+ 	}
+ 	return false;
 }
  console.log("working");
+function checkForNum(n){
+	return !isNaN(parseFloat(n)) && isFinite(n);
+}
